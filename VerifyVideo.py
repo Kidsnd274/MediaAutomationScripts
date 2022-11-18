0186bb6e901263a04a29abbd051dd2581b45620c
Kidsnd274 <samuel_tee_gy@hotmail.com>
2022-11-18 14:46:19 +0800

    Update VerifyVideo script with additional flags

--- VerifyVideo.py
+++ VerifyVideo.py
@@ -10,6 +10,8 @@ import time
 # Command Line Arguments
 parser = argparse.ArgumentParser(description="Script to check for corrupted videos")
 parser.add_argument('-f', '--folder', type=str, metavar='', required=False, help="Folder containing the videos")
+parser.add_argument('-r', '--recursive', action='store_true', required=False, help="Recursively check folders and sub-folders")
+parser.add_argument('-s', '--staxrip', action='store_true', required=False, help="Only includes videos with filename suffix '_new'")
 args = parser.parse_args()
 
 
@@ -26,10 +28,36 @@ if args.folder is None:
     video_directory = current_directory
 else:
     video_directory = pathlib.Path(args.folder).absolute()
+    
+if args.staxrip:
+    staxrip_bool = True
+else:
+    staxrip_bool = False
+    
+if args.recursive:
+    recursive_bool = True
+else:
+    recursive_bool = False
 
 print("Folder:", video_directory.resolve())
 
-video_files = [x for x in video_directory.iterdir() if (x.is_file() and x.suffix in video_file_extensions)]
+video_files = []
+
+if recursive_bool:
+    for i in video_directory.glob('**/*'):
+        cond1 = i.is_file()
+        cond2 = i.suffix in video_file_extensions
+        cond3 = i.stem.endswith("_new") or (not staxrip_bool) # Check Truth Table
+        if (cond1 and cond2 and cond3): # Remove cond3 to remove "_new" check
+            video_files.append(i)
+else: # Check only current folder
+    for i in video_directory.iterdir():
+        cond1 = i.is_file()
+        cond2 = i.suffix in video_file_extensions
+        cond3 = i.stem.endswith("_new") or (not staxrip_bool) # Check Truth Table
+        if (cond1 and cond2 and cond3):
+            video_files.append(i)
+
 if (not video_files):
     print("No video files found")
     exit()
